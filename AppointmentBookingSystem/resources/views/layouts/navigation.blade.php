7987530fb64243f7d56d71f9331bcfcec072ab8d
Arun Basnet <madristarun7@gmail.com>
2023-10-12 14:56:22 +0545

    controller

--- AppointmentBookingSystem/resources/views/layouts/navigation.blade.php
+++ AppointmentBookingSystem/resources/views/layouts/navigation.blade.php
@@ -28,6 +28,22 @@
                     </p>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="{{ route('users.index') }}" class="nav-link">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>
+                        {{ __('Doctors') }}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('users.index') }}" class="nav-link">
+                    <i class="nav-icon fas fa-calendar"></i>
+                    <p>
+                        {{ __('Appointment') }}
+                    </p>
+                </a>
+            </li>
 
             <li class="nav-item">
                 <a href="{{ route('about') }}" class="nav-link">
@@ -38,21 +54,7 @@
                 </a>
             </li>
 
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-circle nav-icon"></i>
-                    <p>
-                        Two-level menu
-                        <i class="fas fa-angle-left right"></i>
-                    </p>
-                </a>
-                <ul class="nav nav-treeview" style="display: none;">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Child menu</p>
-                        </a>
-                    </li>
+            
                 </ul>
             </li>
         </ul>
