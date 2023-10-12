34e4cfccc259e69a229f9c671b3cf4b990e41719
Arun Basnet <madristarun7@gmail.com>
2023-10-12 10:29:20 +0545

    relationship_defined

--- AppointmentBookingSystem/app/Models/Doctors.php
+++ AppointmentBookingSystem/app/Models/Doctors.php
@@ -10,5 +10,11 @@ class Doctors extends Model
     use HasFactory;
     protected $fillable = ['users_id','fname', 'lname','lisence_no','contact','address', 'gender','dob','specialization', 'photo',];
 
+    public function experience() {
+        return $this->hasMany(Experience::class);
+    }
 
+    public function education() {
+        return $this->hasMany(Education::class);
+    }
 }
