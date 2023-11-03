6206ae609d4ae78aafe07b579a2fab947894349e
unknown <madristarun7@gmail.com>
2023-11-03 17:35:28 +0545

    wizardform

--- AppointmentBookingSystem/public/js/wizardform.js
+++ AppointmentBookingSystem/public/js/wizardform.js
@@ -1,47 +1,46 @@
-function toggleFormOne(){
-   var info = document.getElementById('basicinfo');
-   var education = document.getElementById("education");
+// wizardform.js
+function toggleFormOne() {
+    var info = document.getElementById('basicinfo');
+    var education = document.getElementById("education");
 
-   if (info.style.display === "block") {
-       info.style.display = "none";
-       education.style.display = "block";
-   } else {
-       info.style.display = "block";
-       education.style.display = "none"
-       experience.style.display = "none"
-   }
+    if (info.style.display === "block") {
+        info.style.display = "none";
+        education.style.display = "block";
+    } else {
+        info.style.display = "block";
+        education.style.display = "none";
+        experience.style.display = "none";
+    }
 }
 
-function toggleFormTwo(){
-   var education = document.getElementById("education");
-   var experience = document.getElementById("experience");
+function toggleFormTwo() {
+    var education = document.getElementById("education");
+    var experience = document.getElementById("experience");
 
-   if (education.style.display === "block") {
-       education.style.display = "none";
-       experience.style.display = "block";
-   } else {
-       education.style.display = "block";
-       experience.style.display = "none"
-   }
+    if (education.style.display === "block") {
+        education.style.display = "none";
+        experience.style.display = "block";
+    } else {
+        education.style.display = "block";
+        experience.style.display = "none";
+    }
 }
 
-function toggleFormThree(){
-   var experience = document.getElementById("experience");
-   var login = document.getElementById("login");
+function toggleFormThree() {
+    var experience = document.getElementById("experience");
+    var login = document.getElementById("login");
 
-   if (experience.style.display === "block") {
-       experience.style.display = "none";
-       login.style.display = "block";
-   } else {
-      experience.style.display = "block";
-      login.style.display = "none"; 
-   }
+    if (experience.style.display === "block") {
+        experience.style.display = "none";
+        login.style.display = "block";
+    } else {
+        experience.style.display = "block";
+        login.style.display = "none";
+    }
 }
 
-
-
-$(function() {
-    $(document).on('click', '.btn-add', function(e) {
+$(function () {
+    $(document).on('click', '.btn-add', function (e) {
         e.preventDefault();
         var controlForm = $('.fvrduplicate:first'),
             currentEntry = $(this).parents('.entry:first'),
@@ -51,7 +50,7 @@ $(function() {
             .removeClass('btn-add').addClass('btn-remove')
             .removeClass('btn-success').addClass('btn-danger')
             .html('<i class="fa fa-minus" aria-hidden="true"></i>');
-    }).on('click', '.btn-remove', function(e) {
+    }).on('click', '.btn-remove', function (e) {
         e.preventDefault();
         $(this).parents('.entry:first').remove();
         return false;
